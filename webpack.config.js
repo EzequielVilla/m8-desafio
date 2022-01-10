9c78b91cc16f0a0427e4cbf3cb3c89b5546585f8
EzequielVilla <ezequiel.n.villa@gmail.com>
2022-01-10 16:44:53 -0300

    delete live-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,17 @@
 const path = require("path");
 const dev = process.env.NODE_ENV == "development";
 const webpack = require("webpack");
-const liveServer = require("live-server");
+// const liveServer = require("live-server");
 const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
 const dotenv = require("dotenv");
 dotenv.config();
 var colors = require("colors");
 colors.disable();
-if (dev) {
-    liveServer.start({
-        file: "index.html",
-    });
-}
+// if (dev) {
+//     liveServer.start({
+//         file: "index.html",
+//     });
+// }
 const cssRulesModule = {
     test: /\.css?$/i,
     use: [
